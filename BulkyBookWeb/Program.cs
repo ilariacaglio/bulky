157ff9086e051061729ee194af37e2568d98ae6c
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-01-24 11:01:59 +0100

    crea_db

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,12 +1,16 @@
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
