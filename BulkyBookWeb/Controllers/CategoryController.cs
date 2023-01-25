74bc69440946c785c890877e808f6f525f985b2a
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-01-25 09:09:30 +0100

    Category

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Controllers;
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        //recupero le info dal db
+        IEnumerable<Category> objCategoryList = _db.Categories;
+        return View(objCategoryList);
+    }
+}
